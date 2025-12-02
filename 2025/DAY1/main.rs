ff165c1fd712e7abb6e7184dedb6f703ce6a13da
Tristan Russell <tristanrussell2000@gmail.com>
2025-12-01 16:18:17 -0800

    2025 Day 1 Part 1

---
+++ 2025/DAY1/main.rs
@@ -0,0 +1,34 @@
+use std::fs;
+
+fn main() {
+    // --snip--
+    let file_path = "./inputs/dial1.txt";
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    let turns: Vec<&str> = contents.trim().split("\n").collect();
+    let mut dial = 50;
+    let mut count_zeros = 0;
+    
+    for turn in turns {
+        let num_opt = (&turn[1..]).parse::<i32>();
+        let num_int = match num_opt {
+            Ok(input_int) => input_int,
+            Err(e) => {
+                println!("please input a number ({})", e);
+                return;
+            }
+        };
+        match &turn[0..1] {
+            "R" => { dial = (dial + num_int).rem_euclid(100); }
+            "L" => { dial = (dial - num_int).rem_euclid(100); }
+            &_ => { println!("each line must start with R or L"); return; }
+        }
+        if dial == 0 {
+            count_zeros += 1;
+        }
+    }
+
+    println!("{count_zeros}");
+}
