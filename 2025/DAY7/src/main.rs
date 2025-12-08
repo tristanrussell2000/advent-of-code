a94a6503aa5e8e4df8e056ea3d6383e694521fdf
Tristan Russell <tristanrussell2000@gmail.com>
2025-12-08 12:28:47 -0800

    2025 Day 7 Part 2 - Remove Hashmap, use Vec

--- 2025/DAY7/src/main.rs
+++ 2025/DAY7/src/main.rs
@@ -1,7 +1,6 @@
-use std::cell::{Ref, RefCell};
+use std::cell::{RefCell};
 use std::fs;
 use std::time::Instant;
-use std::collections::HashMap;
 use std::ops::AddAssign;
 
 fn main() {
@@ -13,25 +12,22 @@ fn main() {
         row.as_bytes().to_owned()
     }).collect();
     
-    let mut beam_pos: HashMap<usize, RefCell<u64>> = HashMap::new();
-    for i in 0..rows[0].len() {
-        beam_pos.insert(i, RefCell::new(0));
-    }
+    let mut beam_pos: Vec<RefCell<u64>> = vec![RefCell::new(0); rows[0].len()];
     let s_pos = rows[0].iter().position(|&s| s == b'S' ).unwrap();
-    beam_pos.insert(s_pos, RefCell::new(1));
+    beam_pos[s_pos] = RefCell::new(1);
     
     rows[1..].iter().for_each(|row| {
-        for (&pos, paths) in beam_pos.iter() {
+        for (pos, paths) in beam_pos.iter().enumerate() {
             let num_paths = *paths.borrow();
             if row[pos] == b'^' && num_paths > 0 {
-                *beam_pos.get(&pos).unwrap().borrow_mut() = 0;
-                beam_pos.get(&(pos - 1)).unwrap().borrow_mut().add_assign(num_paths);
-                beam_pos.get(&(pos + 1)).unwrap().borrow_mut().add_assign(num_paths);
+                *beam_pos[pos].borrow_mut() = 0;
+                beam_pos[pos-1].borrow_mut().add_assign(num_paths);
+                beam_pos[pos+1].borrow_mut().add_assign(num_paths);
             }
         }
     });
     
-    let paths = beam_pos.iter().map(|(_, splits)| splits).fold(0, |acc, x| acc + *x.borrow());
+    let paths = beam_pos.iter().fold(0, |acc, x| acc + *x.borrow());
     
     let duration = start.elapsed();
     println!("Elapsed time: {:?}", duration);
