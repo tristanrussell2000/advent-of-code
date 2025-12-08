f1f12aea10006e13d3ae3d9d86f36cdabc2557d6
Tristan Russell <tristanrussell2000@gmail.com>
2025-12-07 16:38:22 -0800

    2025 Day 7 Part 2

--- 2025/DAY7/src/main.rs
+++ 2025/DAY7/src/main.rs
@@ -12,26 +12,28 @@ fn main() {
         row.as_bytes().to_owned()
     }).collect();
     
-    let mut splits = 0;
-    let mut beam_pos: HashMap<usize, bool> = HashMap::new();
+    let mut beam_pos: HashMap<usize, u64> = HashMap::new();
     let s_pos = rows[0].iter().position(|&s| s == b'S' ).unwrap();
-    beam_pos.insert(s_pos, true);
+    beam_pos.insert(s_pos, 1);
     
     rows[1..].iter().for_each(|row| {
-        //println!("{row:?}");
-        let curr_entries: Vec<(usize, bool)>= beam_pos.iter().map(|(&x, &y)| (x, y)).collect();
-        for (pos, filled) in curr_entries {
-            if row[pos] == b'^' && filled == true {
-                splits += 1;
-                beam_pos.insert(pos, false);
-                beam_pos.insert(pos - 1, true);
-                beam_pos.insert(pos + 1, true);
+        let curr_entries: Vec<(usize, u64)>= beam_pos.iter().map(|(&x, &y)| (x, y)).collect();
+        for (pos, paths) in curr_entries {
+            if row[pos] == b'^' && paths > 0 {
+                beam_pos.insert(pos, 0);
+                beam_pos.entry(pos - 1)
+                    .and_modify(|x| *x += paths)
+                    .or_insert(paths);
+                beam_pos.entry(pos + 1)
+                    .and_modify(|x| *x += paths)
+                    .or_insert(paths);
             }
         }
     });
     
+    let paths = beam_pos.iter().map(|(_, &splits)| splits).fold(0, |acc, x| acc + x);
     
     let duration = start.elapsed();
     println!("Elapsed time: {:?}", duration);
-    println!("Splits: {}", splits);
+    println!("Splits: {}", paths);
 }
