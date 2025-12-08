36e1540dd75dbcefb3f2be32320936ef9c02f417
Tristan Russell <tristanrussell2000@gmail.com>
2025-12-07 16:51:16 -0800

    2025 Day 7 Part 2 Again but Different

--- 2025/DAY7/src/main.rs
+++ 2025/DAY7/src/main.rs
@@ -1,7 +1,8 @@
+use std::cell::{Ref, RefCell};
 use std::fs;
 use std::time::Instant;
 use std::collections::HashMap;
-
+use std::ops::AddAssign;
 
 fn main() {
     let file_path = "./inputs/input1.txt";
@@ -12,26 +13,25 @@ fn main() {
         row.as_bytes().to_owned()
     }).collect();
     
-    let mut beam_pos: HashMap<usize, u64> = HashMap::new();
+    let mut beam_pos: HashMap<usize, RefCell<u64>> = HashMap::new();
+    for i in 0..rows[0].len() {
+        beam_pos.insert(i, RefCell::new(0));
+    }
     let s_pos = rows[0].iter().position(|&s| s == b'S' ).unwrap();
-    beam_pos.insert(s_pos, 1);
+    beam_pos.insert(s_pos, RefCell::new(1));
     
     rows[1..].iter().for_each(|row| {
-        let curr_entries: Vec<(usize, u64)>= beam_pos.iter().map(|(&x, &y)| (x, y)).collect();
-        for (pos, paths) in curr_entries {
-            if row[pos] == b'^' && paths > 0 {
-                beam_pos.insert(pos, 0);
-                beam_pos.entry(pos - 1)
-                    .and_modify(|x| *x += paths)
-                    .or_insert(paths);
-                beam_pos.entry(pos + 1)
-                    .and_modify(|x| *x += paths)
-                    .or_insert(paths);
+        for (&pos, paths) in beam_pos.iter() {
+            let num_paths = *paths.borrow();
+            if row[pos] == b'^' && num_paths > 0 {
+                *beam_pos.get(&pos).unwrap().borrow_mut() = 0;
+                beam_pos.get(&(pos - 1)).unwrap().borrow_mut().add_assign(num_paths);
+                beam_pos.get(&(pos + 1)).unwrap().borrow_mut().add_assign(num_paths);
             }
         }
     });
     
-    let paths = beam_pos.iter().map(|(_, &splits)| splits).fold(0, |acc, x| acc + x);
+    let paths = beam_pos.iter().map(|(_, splits)| splits).fold(0, |acc, x| acc + *x.borrow());
     
     let duration = start.elapsed();
     println!("Elapsed time: {:?}", duration);
