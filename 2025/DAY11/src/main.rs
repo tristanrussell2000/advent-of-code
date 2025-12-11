7cc7bfa429651439b289228b9f1adb0fbb71c999
Tristan Russell <tristanrussell2000@gmail.com>
2025-12-10 23:18:57 -0800

    2025 Day 11 Part 2 with bytes

--- 2025/DAY11/src/main.rs
+++ 2025/DAY11/src/main.rs
@@ -2,14 +2,32 @@ use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
 
-fn search<'a>(curr: &'a str, paths_map: &'a HashMap<String, Vec<String>>) -> u64 {
-    if curr == "out" { return 1; }
-    let mut total = 0;
-    let conns = paths_map.get(curr).unwrap();
-    
-    for conn in conns {
-        total += search(conn, paths_map);
+fn str_to_code(s: &str) -> u32 {
+    match s.as_bytes() {
+        [a, b, c] => {
+            ((*a as u32) << 16) | (*b as u32) << 8 | (*c as u32)
+        }
+        _ => panic!()
     }
+}
+
+fn search<'a>(curr: u32, paths_map: &'a HashMap<u32, Vec<u32>>, sol_map: &'a mut HashMap<(u32, bool, bool), u64>, seen_fft: bool, seen_dac: bool) -> u64 {
+    if let Some(res) = sol_map.get(&(curr, seen_fft, seen_dac)) {
+        return *res;
+    }
+    if curr == str_to_code("out") { 
+        if seen_fft && seen_dac {
+            return 1;
+        }
+        return 0; 
+    }
+    let mut total = 0;
+    let conns = paths_map.get(&curr).unwrap();
+    
+    for &conn in conns {
+        total += search(conn, paths_map, sol_map,seen_fft || curr == str_to_code("fft"), seen_dac || curr == str_to_code("dac") );
+    }
+    sol_map.insert((curr, seen_fft, seen_dac), total);
     total
 }
 fn main() {
@@ -17,18 +35,18 @@ fn main() {
     let file_path = "./inputs/input1.txt";
     let input = fs::read_to_string(file_path).unwrap();
 
-    let mut reactor_map: HashMap<String, Vec<String>> = HashMap::new();
+    let mut reactor_map: HashMap<u32, Vec<u32>> = HashMap::new();
     for line in input.trim().split("\n") {
         let mut reactor_conns = line.split(":");
         let reactor = reactor_conns.next().unwrap();
         
         let conns = reactor_conns.next().unwrap();
-        let conn_vec: Vec<String> = conns.trim().split(" ").map(|s| s.to_string()).collect();
+        let conn_vec: Vec<u32> = conns.trim().split(" ").map(|s| str_to_code(s)).collect();
         
-        reactor_map.insert(reactor.to_string(), conn_vec);
+        reactor_map.insert(str_to_code(reactor), conn_vec);
     }
-    
-    let total = search("you", &reactor_map);
+    let mut sol_map: HashMap<(u32, bool, bool), u64> = HashMap::new();
+    let total = search(str_to_code("svr"), &reactor_map, &mut sol_map, false, false);
 
     let duration = start.elapsed();
     println!("Elapsed time: {:?}", duration);
